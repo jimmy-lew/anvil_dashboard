eb27ad31c0772fc70ec2c7380c6e9721a232d49f
jimmy-lew <jimmylew9779@gmail.com>
2025-12-31 16:37:14 +0800

    feat: migrate environment filter to own component

---
+++ app/components/log/filters/Environment.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ChevronRight } from 'lucide-vue-next'
+
+const isOpen = ref(false)
+</script>
+
+<template>
+  <Collapsible
+    v-model:open="isOpen"
+    class="flex flex-col gap-2"
+  >
+    <div class="flex items-center justify-start gap-2">
+      <CollapsibleTrigger as-child>
+        <Button variant="ghost" class="w-full flex justify-start">
+          <ChevronRight />
+          <span class="sr-only">Toggle</span>
+          <h4 class="text-sm font-semibold">
+            Environment
+          </h4>
+        </Button>
+      </CollapsibleTrigger>
+    </div>
+    <CollapsibleContent class="flex flex-col gap-2">
+      <div class="rounded-md border px-4 py-2 font-mono text-sm">
+        @radix-ui/colors
+      </div>
+      <div class="rounded-md border px-4 py-2 font-mono text-sm">
+        @stitches/react
+      </div>
+    </CollapsibleContent>
+  </Collapsible>
+  <div />
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,6 @@
 import { ChevronDownIcon, ChevronRight, EllipsisVerticalIcon, FilterIcon, PlayCircleIcon, RefreshCwIcon } from 'lucide-vue-next'
 
 const isOpen = ref(false)
-const isOpen2 = ref(false)
 
 const panelOpen = ref(true)
 
@@ -75,30 +74,7 @@ onUnmounted(() => { log_stream_client.disconnect() })
           <div class="flex flex-col pt-2">
             <LogFiltersTime />
             <LogFiltersLevels />
-            <Collapsible
-              v-model:open="isOpen2"
-              class="flex flex-col gap-2"
-            >
-              <div class="flex items-center justify-start gap-2">
-                <CollapsibleTrigger as-child>
-                  <Button variant="ghost" class="w-full flex justify-start">
-                    <ChevronRight />
-                    <span class="sr-only">Toggle</span>
-                    <h4 class="text-sm font-semibold">
-                      Environment
-                    </h4>
-                  </Button>
-                </CollapsibleTrigger>
-              </div>
-              <CollapsibleContent class="flex flex-col gap-2">
-                <div class="rounded-md border px-4 py-2 font-mono text-sm">
-                  @radix-ui/colors
-                </div>
-                <div class="rounded-md border px-4 py-2 font-mono text-sm">
-                  @stitches/react
-                </div>
-              </CollapsibleContent>
-            </Collapsible>
+            <LogFiltersEnvironment />
           </div>
         </div>
       </div>
