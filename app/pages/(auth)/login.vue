c6377d02903b93a4dc33d1ee321f6388d86e5173
jimmy-lew <jimmylew9779@gmail.com>
2026-01-04 20:00:31 +0800

    feat: baseline updates to auth components

--- app/pages/(auth)/login.vue
+++ app/pages/(auth)/login.vue
@@ -8,9 +8,7 @@ definePageMeta({
   <div class="flex flex-col items-center justify-center gap-6 bg-muted p-6 min-h-svh md:p-10">
     <div class="max-w-sm w-full flex flex-col gap-6">
       <NuxtLink to="#" class="flex items-center self-center gap-2 font-medium">
-        <div class="h-6 w-6 flex items-center justify-center rounded-md bg-primary text-primary-foreground">
-          <Icon name="i-lucide-gallery-vertical-end" class="size-4" />
-        </div>
+        <img src="/icon.svg" class="h-6 w-6">
         Acme Inc.
       </NuxtLink>
       <div class="flex flex-col gap-6">
