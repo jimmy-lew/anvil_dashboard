6111777a1bb228f76687d1cae26025cc2499bd70
jimmy <jimmylew9779@gmail.com>
2026-01-07 21:11:39 +0800

    feat: generalize filter component

---
+++ app/components/log/filters/Base.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ChevronRight } from 'lucide-vue-next'
+
+defineProps<{
+  title: string
+}>()
+
+const isOpen = ref(true)
+</script>
+
+<template>
+  <Collapsible
+    v-model:open="isOpen"
+    class="flex flex-col gap-2"
+  >
+    <div class="flex items-center justify-start gap-2">
+      <CollapsibleTrigger as-child>
+        <Button variant="ghost" class="w-full flex justify-start group">
+          <ChevronRight class="group-data-[state=open]:rotate-90 transition-all duration-300 ease-in-out" />
+          <span class="sr-only">Toggle</span>
+          <h4 class="text-sm font-semibold">
+            {{ title }}
+          </h4>
+        </Button>
+      </CollapsibleTrigger>
+    </div>
+    <CollapsibleContent
+      class="
+        flex flex-col
+        text-base border mb-2 rounded
+        data-[state=closed]:animate-collapsible-up
+        data-[state=open]:animate-collapsible-down
+        transition-all duration-100
+        "
+    >
+      <slot />
+    </CollapsibleContent>
+  </Collapsible>
+</template>
+
+<style scoped>
+
+</style>
