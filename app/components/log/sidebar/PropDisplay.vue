5ebbd874cfe09b3e1f2fbfee418d892b6ec4f701
jimmy-lew <jimmylew9779@gmail.com>
2026-01-17 16:25:50 +0800

    minor: update PropDisplay

--- app/components/log/sidebar/PropDisplay.vue
+++ app/components/log/sidebar/PropDisplay.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-defineProps<{
-  item?: RawLogItem
-}>()
+defineProps<{ item?: RawLogItem }>()
 </script>
 
 <template>
   <div v-if="item" class="flex flex-col border bg-[#0a0a0a] rounded">
     <div class="flex flex-col px-2 py-1">
       <div class="flex flex-col">
-        <div v-for="(v, index) in Object.entries(item)" :key="index" class="border-b py-1 last:border-none">
+        <div v-for="([field, value], index) in Object.entries(item)" :key="index" class="border-b py-1 last:border-none">
           <Button variant="ghost" class="text-base w-full">
             <div class="flex items-center justify-between w-full gap-1">
-              <span class="text-muted-foreground">{{ v[0] }}</span>
-              <span class="pl-6 truncate">{{ v[1] }}</span>
+              <span class="text-muted-foreground">{{ field }}</span>
+              <span class="pl-8 truncate">{{ value }}</span>
             </div>
           </Button>
         </div>
