c3caacc28ea387e54a67340157776876a6ce138a
jimmy-lew <jimmylew9779@gmail.com>
2026-01-18 08:49:33 +0800

    feat: migrate out of sidebar component and implement panel with animations

--- app/components/log/filters/index.vue
+++ app/components/log/filters/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+const { isFiltersActive } = useLogFilters()
 </script>
 
 <template>
-  <div class="hidden md:block dark:bg-black border-r relative h-full">
-    <div class="sticky">
+  <div
+    :class="isFiltersActive ? 'left-0' : '-left-[270px]'"
+    class="hidden md:block dark:bg-black border-r relative h-full w-full transition-all ease-in-out duration-200"
+  >
+    <div class="fixed w-[270px]">
       <div class="flex flex-col h-full p-3">
         <div class="flex flex-initial items-center justify-between">
           <span class="text-base p-3">
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ChevronDownIcon } from 'lucide-vue-next'
-
-const isOpen = ref(false)
-const panelOpen = ref(true)
+const isOpen = ref(true)
 
 const log_header = useTemplateRef('log_header')
 const log_list = useTemplateRef('log_list')
@@ -22,13 +19,16 @@ defineShortcuts({
 </script>
 
 <template>
-  <div class="grid h-full relative" :class="isFiltersActive ? 'grid-cols-1 md:grid-cols-[270px_calc(100%-270px)]' : 'grid-cols-1'">
-    <LogFilters v-if="isFiltersActive" />
-    <div class="flex flex-col relative h-full">
-      <div class="flex flex-col items-stretch justify-start flex-initial sticky border-b overflow-x-auto">
+  <div
+    class="grid h-full relative"
+    :class="isFiltersActive ? 'grid-cols-1 md:grid-cols-[270px_calc(100%-270px)]' : 'grid-cols-[0px_100%]'"
+  >
+    <LogFilters />
+    <div class="flex flex-col relative h-full overflow-hidden">
+      <div class="fixed top-[calc(--header-height+8px)] z-50 w-full bg-background flex flex-col items-stretch justify-start flex-initial border-b overflow-x-auto">
         <div class="px-3 pt-3">
           <LogSearch />
-          <LogChart range="2h" />
+          <LogChart range="6h" />
           <div ref="log_header" class="max-w-screen overflow-x-hidden no-scrollbar">
             <div class="flex flex-row items-center justify-stretch flex-initial text-base h-10 min-w-4xl">
               <div class="pl-5 w-53">
@@ -44,29 +44,23 @@ defineShortcuts({
           </div>
         </div>
       </div>
-      <div ref="log_list" class="relative overflow-x-auto md:overflow-x-hidden flex-[1_1_0%]" @scroll.passive="handle_scroll">
-        <ol class="flex flex-col items-stretch justify-start flex-auto px-1 min-w-4xl h-full">
-          <LogItem v-for="(log_item, index) in logs" :key="log_item.log_id" :item="log_item" :index @select="isOpen = true" />
-        </ol>
-        <SidebarProvider v-model:open="isOpen" class="min-h-0" style="--sidebar-width: max(min(400px,50vw), 350px);">
-          <LogItemSidebar @close="isOpen = false" />
-        </SidebarProvider>
-      </div>
+      <div class="relative flex-1 min-h-0 overflow-hidden">
+        <div
+          ref="log_list" class="pt-36 h-full overflow-x-auto md:overflow-x-hidden overflow-y-auto relative"
+          @scroll.passive="handle_scroll"
+        >
+          <ol class="flex flex-col items-stretch justify-start flex-auto px-1 min-w-4xl h-full">
+            <LogItem
+              v-for="(log_item, index) in logs" :key="log_item.log_id" :item="log_item" :index
+              @select="isOpen = true"
+            />
+          </ol>
 
-      <!-- Technically left should be 270px -->
-      <div v-if="isOpen" :style="{ height: panelOpen ? '360px' : '50px' }" class="fixed block bottom-0 right-[max(min(400px,50vw),350px)] left-76 border-t">
-        <div class="flex h-full flex-1 flex-col transition-all sm:flex-initial">
-          <div class="flex items-center justify-between border-b p-2 bg-background">
-            <span class="px-3 py-1.5">Logs</span>
-            <Button variant="ghost" size="icon" @click="panelOpen = !panelOpen">
-              <ChevronDownIcon :size="16" class="" />
-            </Button>
-          </div>
-          <div v-if="panelOpen" class="flex h-full items-center justify-center bg-black p-8 text-sm text-muted-foreground">
-            No items found
-          </div>
+          <LogItemSidebar v-model:open="isOpen" />
         </div>
       </div>
+
+      <LogPanel v-model:open="isOpen" />
     </div>
   </div>
 </template>
